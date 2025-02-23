4eccce9687c1d54ca8287f46c7e698760a71e10e
hudics <piotrchud@interia.pl>
2025-02-23 13:19:09 +0100

    added additional files

---
+++ CardActionsService.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CardActionsService.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
